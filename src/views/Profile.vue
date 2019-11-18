e6fad980bdcb2567b3e252f48c0504124dc5da5c
Henrique Marcio Leonel <henriquemleonel@outlook.com>
2019-11-18 12:32:40 -0400

    implementando perfil com q-cards

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,12 +5,72 @@
         </div>
       </div>
       <div class="row">
-        <q-card style="height:400px; width: 400px; background-color: #808080; display: flex; flex-direction: column; align-items: center;
-        margin-top: 90px;">
-            <!--<q-img
+        <div class="q-pa-md row items-start q-gutter-md no-wrap" style="width: 100%;">
+          <q-card class="perfil-card" style="width: 380px;">
+            <q-img src="https://cdn.quasar.dev/img/avatar2.jpg" style="height: 300px;">
+              <div class="absolute-bottom" align="center">
+                <div class="text-h6">{{userName}}</div>
+                <div class="text-subtitle2">{{userCategory}}</div>
+              </div>
+            </q-img>
+
+            <q-card-actions class="bg-white" vertical align="right">
+              <div class="row">
+                <span class="user-email" style="margin-top: 15px;"> {{ userEmail }} </span>
+                <q-btn flat style="margin-left: 40px; margin-right: 10px; margin-top: 10px; width: 70px;">
+                  <q-icon name="edit" size="20px"></q-icon>
+                </q-btn>
+              </div>
+              <div class="row" style="">
+                <span class="user-password" style="margin-top: 15px;"> {{ userPassword }} </span>
+                <q-btn flat style="margin-left: 40px; margin-top: 10px; margin-right: 10px; width: 70px;">
+                  <q-icon name="edit" size="20px"></q-icon>
+                </q-btn>
+              </div>
+            </q-card-actions>
+          </q-card>
+          <q-card class="pin-card" style="min-width: 25%">
+            <q-card-section class="bg-yellow text-black">
+              <div class="text-h6">Adicionar Pin</div>
+              <div class="text-subtitle2">Faça parte da Cartografia</div>
+            </q-card-section>
+
+            <q-card-section class="" v-if="opemBoxPin" style="height: 270px; width: 100%;">
+              <span class="subtitle" style="font-size: 12px; position: relative; top: 10px;"> Este pin te localiza no mapa com os dados inseridos abaixo</span>
+              <div class="info-field" v-if="" style="display: flex; flex-direction: row; width: 100%; margin-top: 20px;">
+                <div class="collumn" style="width: 200px;">
+                  <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Nome do Pin</span>
+                  <q-input class="input" v-model="text" label="ex: Brava" color="white"/>
+                </div>
+                <div class="collumn" style="width: 300px; margin-left: 20px;">
+                  <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Email </span>
+                  <q-input class="input" v-model="text" label="ex: fulano@email.com" color="white"/>
+                </div>
+              </div>
+              <div class="info-field" v-if="" style="display: flex; flex-direction: row; width: 100%; margin-top: 20px;">
+                <div class="column" style="width: 250px">
+                  <span class="text-info" style="font-size: 12px; position: relative; top: 10px;">Rua </span>
+                  <q-input class="input" v-model="text" label="ex: Rua do dinar" color="white"/>
+                </div>
+              </div>
+            </q-card-section>
+
+            <q-separator />
+            <q-card-actions align="right" style="background: #ff9969">
+              <q-btn flat @click="opemBoxPin=true" v-if="opemBoxPin===false">Criar Pin</q-btn>
+              <q-btn flat v-if="opemBoxPin" @click="">Finalizar</q-btn>
+              <q-btn flat v-if="opemBoxPin" @click="opemBoxPin=false">Cancelar</q-btn>
+            </q-card-actions>
+          </q-card>
+
+        </div>
+
+        <!--<q-card style="height:400px; width: 400px; background-color: #808080; display: flex; flex-direction: column; align-items: center;
+        margin-top: 90px; margin-left: 30px;">
+            <q-img
               src="logo.png"
               style="width: 150px; height: 150px; position: relative; top: -40px;">
-            </q-img>-->
+            </q-img>
             <div class="" style="width: 150px; height: 150px; position: relative; top: -40px; background: yellow;">
 
             </div>
@@ -30,9 +90,9 @@
                 </q-btn>
               </div>
             </div>
-        </q-card>
-        <q-card style="width: 400px; background-color: #808080; display: flex; flex-direction: column; align-items: center;
-        margin-top: 90px; margin-left: 30px;">
+        </q-card>-->
+        <!--<q-card style="width: 400px; background-color: #808080; display: flex; flex-direction: column; align-items: center;
+        margin-top: 0px; margin-left: 30px;">
           <span class="title" style="font-size: 30px; position: relative; top: 10px;">ADICIONAR PIN</span>
           <span class="subtitle" style="font-size: 12px; position: relative; top: 10px;"> Este pin te localiza no mapa com os dados inseridos abaixo</span>
           <div class="hidden-field" v-if="" style="display: flex; flex-direction: column; width: 80%; margin-top: 20px;">
@@ -43,7 +103,7 @@
             <span class="text-info" style="font-size: 12px; position: relative; top: 10px;">Rua </span>
             <q-input class="input" v-model="text" label="ex: Rua do dinar" color="white"/>
           </div>
-        </q-card>
+        </q-card>-->
       </div>
     </div>
   </q-page>
@@ -58,30 +118,31 @@ export default {
       userCategory: 'Cinema e AudioVisual',
       userEmail: 'JulianaTrujillo@email.com',
       userPassword: '**********',
-      openHiddenPin: false,
+      opemBoxPin: false,
+      addPin: false
     };
   },
 };
 </script>
-<style lang="stylus" scoped>
-.row {
-  display: flex;
-  flex-direction: row;
-}
-.collumn {
-  display: flex;
-  flex-direction: column;
-  border-radius: 5px;
-}
-.nav {
-  width: 100px;
-  height: 100%;
-  background-color: yellow;
-}
-.text-info {
-  margin: 5px;
-}
-.input {
-  margin: 5px;
-}
+<style lang="sass" scoped>
+.row
+  display: flex
+  flex-direction: row
+
+.collumn
+  display: flex
+  flex-direction: column
+  border-radius: 5px
+
+.nav
+  width: 100px
+  height: 100%
+  background-color: yellow
+
+.text-info
+  margin: 5px
+
+.input
+  margin: 5px
+
 </style>
