34862702d49f626bcf1deae6f84b21b385d5780d
Henrique Marcio Leonel <henriquemleonel@outlook.com>
2019-11-19 22:05:08 -0400

    estética da pagina cadastro

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,22 @@
 <template>
   <q-page padding>
-    <div class="collumn">
-      <span class="title">Cadastro</span>
-      <span class="subtitle"> O cadastro permitirá:
-        MAPA_Adicionar um pin no mapa (sua localização e as informações que você inserir serão visto por todos).
-        Para o Fórum Municipal de Cultura é muito importante que você, sendo da classe artística, produtor cultural, instituição ou responsável
-        por estabelecimento que promove arte e cultura na cidade, participe desta plataforma. A cartografia da cultura é uma maneira de dar voz e visibilidade
-        A todos que produzem arte e cultura, sem hierarquias e com transparência. Cada cadastro permitirá que você insira um pin.
-        AGENDA_Inserir na agenda os eventos que você irá participar ou produzir em Campo Grande Além de ser mais um local de divulgação do seu trabalho,
-        o visitante ao acessar a plataforma, poderá visualizar os eventos que acontecerão nos próximos dias, reunidos num só lugar.
-        DIÁLOGO_Propor debates ou participar de um debate que acontece online, proposto pelo Fórum Municipal de Cultura ou pelos participantes,
-        entre a produtores artísticos e culturais da cidade. </span>
-
-        <div class="row">
-          <div class="collumn" style="width: 200px;">
+    <div class="collumn" style="align-content: center">
+        <div class="">
+          <span class="title" style="margin-bottom: 20px;">Cadastro</span>
+          <span class="subtitle">
+            O cadastro permitirá:
+            MAPA_Adicionar um pin no mapa (sua localização e as informações que você inserir serão visto por todos).
+            Para o Fórum Municipal de Cultura é muito importante que você, sendo da classe artística, produtor cultural, instituição ou responsável
+            por estabelecimento que promove arte e cultura na cidade, participe desta plataforma. A cartografia da cultura é uma maneira de dar voz e visibilidade
+            A todos que produzem arte e cultura, sem hierarquias e com transparência. Cada cadastro permitirá que você insira um pin.
+            AGENDA_Inserir na agenda os eventos que você irá participar ou produzir em Campo Grande Além de ser mais um local de divulgação do seu trabalho,
+            o visitante ao acessar a plataforma, poderá visualizar os eventos que acontecerão nos próximos dias, reunidos num só lugar.
+            DIÁLOGO_Propor debates ou participar de um debate que acontece online, proposto pelo Fórum Municipal de Cultura ou pelos participantes,
+            entre a produtores artísticos e culturais da cidade.
+          </span>
+        </div>
+        <div class="row" style="widht: 70%; margin-top: 50px;">
+          <div class="collumn" style="width: 300px;">
             <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Nome*</span>
             <q-input class="input" v-model="userName" label="" color="white"/>
           </div>
@@ -27,11 +30,12 @@
           </div>
         </div>
         <div class="row user-data">
-          <div class="collumn" style="width: 300px; margin-left: 20px;">
+          <div class="collumn" style="width: 300px; margin-left: 0px;">
             <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Senha* </span>
             <q-input v-model="password" :type="isPwd ? 'password' : 'text'">
               <template v-slot:append>
                 <q-icon
+                  size="20px"
                   :name="isPwd ? 'visibility_off' : 'visibility'"
                   class="cursor-pointer"
                   @click="isPwd = !isPwd"
@@ -44,6 +48,7 @@
             <q-input v-model="confirmPassword" :type="isPwd ? 'password' : 'text'">
               <template v-slot:append>
                 <q-icon
+                  size="20px"
                   :name="isPwd ? 'visibility_off' : 'visibility'"
                   class="cursor-pointer"
                   @click="isPwd = !isPwd"
@@ -52,7 +57,7 @@
             </q-input>
           </div>
         </div>
-        <div class="column user-Category">
+        <div class="column user-Category" style="margin-top: 50px">
           <span class="title">Identificação ou Área de Atuação</span>
           <span>(A categoria escolhida irá aparecer no seu pin no mapa)</span>
           <div class="q-pa-md">
@@ -81,6 +86,9 @@ export default {
   name: 'PageAbout',
   data () {
     return{
+      userName: '',
+      userEmail: '',
+      userPassword: '',
       isPwd: true,
       userCategory: null,
       secondaryUserCategory: null,
@@ -97,6 +105,9 @@ export default {
         { label: 'Arte Digital', value: 'arteDigital', color: 'orange' }
       ]
     }
+  },
+  methods () {
+
   }
 };
 </script>
@@ -114,6 +125,12 @@ export default {
   font-size: 20px
 
 .title
-  width: 40%
+  font-family: sans-serif
+  font-size: 30px
+  font-weight: 500
   word-wrap: break-word
+
+.subtitle
+  font-size: 18px
+
 </style>
