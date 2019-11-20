fd3e8502429ff857aec9912bc78fda784758dc8c
Henrique Marcio Leonel <henriquemleonel@outlook.com>
2019-11-20 15:14:54 -0400

    estética de cadastro

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,36 +1,35 @@
 <template>
   <q-page padding>
-    <div class="collumn" style="align-content: center">
-        <div class="">
-          <span class="title" style="margin-bottom: 20px;">Cadastro</span>
-          <span class="subtitle">
-            O cadastro permitirá:
-            MAPA_Adicionar um pin no mapa (sua localização e as informações que você inserir serão visto por todos).
-            Para o Fórum Municipal de Cultura é muito importante que você, sendo da classe artística, produtor cultural, instituição ou responsável
-            por estabelecimento que promove arte e cultura na cidade, participe desta plataforma. A cartografia da cultura é uma maneira de dar voz e visibilidade
-            A todos que produzem arte e cultura, sem hierarquias e com transparência. Cada cadastro permitirá que você insira um pin.
-            AGENDA_Inserir na agenda os eventos que você irá participar ou produzir em Campo Grande Além de ser mais um local de divulgação do seu trabalho,
-            o visitante ao acessar a plataforma, poderá visualizar os eventos que acontecerão nos próximos dias, reunidos num só lugar.
-            DIÁLOGO_Propor debates ou participar de um debate que acontece online, proposto pelo Fórum Municipal de Cultura ou pelos participantes,
-            entre a produtores artísticos e culturais da cidade.
-          </span>
+    <div class="row" style="align-content: center">
+        <div class="column context" style="height: 100%; width: 40%; margin-left: 4rem; margin-top: 2rem;">
+          <span class="title" style="margin-bottom: 2rem">Cadastro</span>
+          <p>O cadastro permitirá:</p>
+          <p>MAPA:</p>
+          <p>Adicionar um pin no mapa (sua localização e as informações que você inserir serão visto por todos). Cada cadastro permitirá que você insira um pin.</p>
+          <p>AGENDA:</p>
+          <p>Inserir na agenda os eventos que você irá participar ou produzir em Campo Grande Além de ser mais um local de divulgação do seu trabalho,
+          o visitante ao acessar a plataforma, poderá visualizar os eventos que acontecerão nos próximos dias, reunidos num só lugar.</p>
+          <p>DIÁLOGO:</p>
+          <p>Propor debates ou participar de um debate que acontece online, proposto pelo Fórum Municipal de Cultura ou pelos participantes,
+          entre a produtores artísticos e culturais da cidade.</p>
+          <p>Para o Fórum Municipal de Cultura é muito importante que você, sendo da classe artística, produtor cultural, instituição ou responsável
+          por estabelecimento que promove arte e cultura na cidade, participe desta plataforma. A cartografia da cultura é uma maneira de dar voz e visibilidade
+          A todos que produzem arte e cultura, sem hierarquias e com transparência.</p>
         </div>
-        <div class="row" style="widht: 70%; margin-top: 50px;">
-          <div class="collumn" style="width: 300px;">
+        <div class="column user-data" style="margin-left: 7rem; margin-top: 6rem;">
+          <div class="collumn" style="width: 22rem;">
             <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Nome*</span>
             <q-input class="input" v-model="userName" label="" color="white"/>
           </div>
-          <div class="collumn" style="width: 300px; margin-left: 20px;">
+          <div class="collumn" style="width: 22rem;">
             <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Email* </span>
             <q-input class="input" v-model="userEmail" label="" color="white"/>
           </div>
-          <div class="collumn" style="width: 300px; margin-left: 20px;">
+          <div class="collumn" style="width: 22rem;">
             <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Confirme seu email </span>
             <q-input class="input" v-model="confirmEmail" label="" color="white"/>
           </div>
-        </div>
-        <div class="row user-data">
-          <div class="collumn" style="width: 300px; margin-left: 0px;">
+          <div class="collumn" style="width: 19rem; margin-left: 0px;">
             <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Senha* </span>
             <q-input v-model="password" :type="isPwd ? 'password' : 'text'">
               <template v-slot:append>
@@ -43,7 +42,7 @@
               </template>
             </q-input>
           </div>
-          <div class="collumn" style="width: 300px; margin-left: 20px;">
+          <div class="collumn" style="width: 19rem; margin-left: 0px;">
             <span class="text-info text-black" style="font-size: 12px; position: relative; top: 10px;">Confirme sua senha </span>
             <q-input v-model="confirmPassword" :type="isPwd ? 'password' : 'text'">
               <template v-slot:append>
@@ -56,25 +55,28 @@
               </template>
             </q-input>
           </div>
-        </div>
-        <div class="column user-Category" style="margin-top: 50px">
-          <span class="title">Identificação ou Área de Atuação</span>
-          <span>(A categoria escolhida irá aparecer no seu pin no mapa)</span>
-          <div class="q-pa-md">
-            <q-option-group
-              :options="primaryOptions"
-              label="Notifications"
-              type="radio"
-              v-model="userCategory"
-            />
-            <q-option-group
-              v-if="userCategory==='atuacao'"
-              :options="secondaryOptions"
-              label="Notifications"
-              type="radio"
-              v-model="secondaryUserCategory"
-              style="margin-left: 50px;"
-            />
+          <div class="column user-Category" style="margin-top: 50px">
+            <span class="subtitle">Identificação ou Área de Atuação</span>
+            <span>(A categoria escolhida irá aparecer no seu pin no mapa)</span>
+            <div class="q-pa-md option-group">
+              <q-option-group
+                :options="primaryOptions"
+                label="Notifications"
+                type="radio"
+                v-model="userCategory"
+              />
+              <q-option-group
+                v-if="userCategory==='atuacao'"
+                :options="secondaryOptions"
+                label="Notifications"
+                type="radio"
+                v-model="secondaryUserCategory"
+                style="margin-left: 50px;"
+              />
+            </div>
+          </div>
+          <div class="btn" style="margin-top: 1.2rem">
+            <q-btn filled>Finalizar</q-btn>
           </div>
         </div>
     </div>
